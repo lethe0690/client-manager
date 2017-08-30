99e8678462ccd9cc3a204a50376dbe2db32ef2bf
Lethe <lethe0690@gmail.com>
2017-08-29 23:16:16 -0400

    add - find client with account number add - create client with accounts

--- api/controllers/client.js
+++ api/controllers/client.js
@@ -2,6 +2,7 @@ const dataModel = require('../dataModel/dataModel');
 const auth = require('./auth').auth;
 const errors = require('./errorHandler');
 const moment = require('moment');
+const generateAccountNumber = require('./account').generateAccountNumber;
 
 function getDateBefore(years) {
     return moment().subtract(parseInt(years), 'years').toDate();
@@ -116,4 +117,83 @@ function deleteClient(req, res) {
     })
 }
 
-module.exports = {createClient, getClient, updateClient, deleteClient, getDateBefore};
+function findClientByAccount(req, res) {
+    auth(req, res, ()=> {
+        let an = req.swagger.params["accountNumber"].value;
+
+        return dataModel.account.findOne({number: an}, {
+            // can decide what field to show to the client, right now display everything for simple
+            __v: 0
+        })
+            .then((account)=> {
+                if (!account) {
+                    res.status(404);
+                    return res.json({message: 'account not found'})
+                }
+
+                return Promise.resolve(account.cid);
+            })
+            .then((cid)=> {
+                return dataModel.client.findOne({_id: cid}, {
+                    // can decide what field to show to the client, right now display everything for simple
+                    __v: 0
+                })
+                    .then((client)=> {
+                        if (!client) {
+                            res.status(404);
+                            return res.json({message: 'client not found'})
+                        }
+
+                        return res.json(client);
+                    })
+            })
+            .catch((err)=> {
+                return errors.errorHandler(res, err, 'controllers/client', 500, 'internal error');
+            })
+    });
+}
+
+function createClientWithAccounts(req, res) {
+
+    let accounts;
+    let clientForm = {};
+
+    let form = req.swagger.params.form.value;
+
+    Object.keys(form).forEach((item)=> {
+        if (item === 'accounts') accounts = form[item];
+        else {
+            if (form[item]) clientForm[item] = form[item];
+        }
+    });
+
+    let accountNumbers = generateAccountNumber(accounts.length);
+    let accountsWithNumber = accounts.map((item)=> {
+        item.number = accountNumbers.pop();
+        return item;
+    });
+
+    return dataModel.client.create(clientForm)
+        .then((client)=> {
+            accountsWithNumber.forEach((item)=> {
+                item.cid = client._id;
+            });
+            return dataModel.account.create(accountsWithNumber)
+                .then(()=> {
+                    res.json(client._id);
+                })
+                .catch((err)=> {
+                    //rollback the previous write
+                    dataModel.client.findByIdAndRemove(client._id);
+                    return errors.errorHandler(res, err, 'controllers/client', 500, 'internal error');
+                })
+        })
+        .catch((err)=> {
+            return errors.errorHandler(res, err, 'controllers/client', 500, 'internal error');
+        })
+}
+
+module.exports = {
+    createClient, getClient, updateClient,
+    deleteClient, getDateBefore, findClientByAccount, createClientWithAccounts
+};
