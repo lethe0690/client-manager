f108b09f2fa7f1039857759b0dec5f4496e2dcb0
Lethe <lethe0690@gmail.com>
2017-08-29 23:46:52 -0400

    refactor - config redis setting

--- api/redis/redis.js
+++ api/redis/redis.js
@@ -4,11 +4,11 @@ const config = require("../../config");
 const Redis = require('ioredis');
 
 const redisclient = new Redis({
-    port: 6379,          // Redis port
-    host: '127.0.0.1',   // Redis host
+    port: config.redis_port || 6379,          // Redis port
+    host: config.redis_host || '127.0.0.1',   // Redis host
     family: 4,           // 4 (IPv4) or 6 (IPv6)
-    password: '1234567890',
-    db: 0
+    //password: '',
+    db: config.redis_db || 0
 });
 
 const EXPIRE_TIME = config.redis_ttl;
--- config/config.development.js
+++ config/config.development.js
@@ -3,7 +3,10 @@ module.exports = {
     port: 5000,
 
     db_mongo_connection: 'mongodb://rbc:rbc@ds161503.mlab.com:61503/rbc',
-    
+
+    redis_port: 6379,
+    redis_host: 'localhost',
+    redis_db: 0,
     //unit in sec , 5 min in this case
     redis_ttl: 5 * 60,
 
